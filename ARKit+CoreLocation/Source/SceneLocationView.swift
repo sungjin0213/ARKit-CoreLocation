ecb90d4511568547651bc9d3481b6be062533eab
Stephen Jun <sjun@avaimobile.com>
2018-04-25 14:09:19 -0500

    annotationNode to apply scale adjustment also for scaleRelativeToDistance
    This is so that the labels won't appear so big.

--- ARKit+CoreLocation/Source/SceneLocationView.swift
+++ ARKit+CoreLocation/Source/SceneLocationView.swift
@@ -418,8 +418,17 @@ public class SceneLocationView: ARSCNView, ARSCNViewDelegate {
             var scale: Float
             
             if annotationNode.scaleRelativeToDistance {
-                scale = appliedScale.y
-                annotationNode.annotationNode.scale = appliedScale
+                //Scale it to be an appropriate size so that it can be seen
+                scale = Float(adjustedDistance) * 0.181
+                
+                if distance > 3000 {
+                    scale = scale * 0.75
+                }
+                
+                annotationNode.annotationNode.scale = SCNVector3(x: appliedScale.x * scale, y: appliedScale.y * scale, z: appliedScale.z * scale)
+                
+//                scale = appliedScale.y
+//                annotationNode.annotationNode.scale = appliedScale
             } else {
                 //Scale it to be an appropriate size so that it can be seen
                 scale = Float(adjustedDistance) * 0.181
